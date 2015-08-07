29d60569ff990c03ced99a9022d97f7a6f602302
Lars Hollingsed <larshollingsed@gmail.com>
2015-08-07 12:13:33 -0500

    to link posts.js(post factory) to database

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,7 +1,19 @@
 angular.module('flapperNews')
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
+.factory(
+  'posts', [
+  '$http',
+  function($http){
+    var o = {
+      posts: []
+    };
+    
+    o.getAll = function() {
+      return $http.get('/posts.json').success(function(data){
+        angular.copy(data, o.posts);
+      });
+    };
+    
+    
+    return o;
+    
+  }])
